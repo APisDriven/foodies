fdb8852218f3930eb2ecb6adccdd98cbaf92ac20
Enemirovskaya <elenanemirovskaya1984@gmail.com>
2022-12-24 18:45:22 -0500

    rearanged server.js

--- server.js
+++ server.js
@@ -5,24 +5,10 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
-const sequelize = require('./config/connection');
-const SequelizeFood = require('connect-session-sequelize')(session.Store);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(routes);
-
-// Create a new sequelize store using the express-session package
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 
@@ -39,6 +25,17 @@ const sess = {
 
 // Add express-session and store as Express.js middleware
 app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
 
 
 sequelize.sync({ force: false }).then(() => {
