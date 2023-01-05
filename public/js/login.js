34946dd22acbb8144f125116a860e0c6a76a8e0d
apisdriven <apisdriven@gmail.com>
2023-01-04 19:56:59 -0500

    Changed login and register destination

--- public/js/login.js
+++ public/js/login.js
@@ -15,7 +15,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
       alert('Failed to log in');
     }
@@ -45,7 +45,7 @@ if (signupEmail.value && signupName.value && signupPw.value) {
 
   if (response.ok) {
     
-    document.location.replace('/');
+    document.location.replace('/dashboard');
   } else {
     alert('Failed to log in');
   }
