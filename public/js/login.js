2506993fb9e589a4e2a6ab0a178e89cc7ec3c285
apisdriven <apisdriven@gmail.com>
2023-01-07 12:10:36 -0500

    fixed register button

--- public/js/login.js
+++ public/js/login.js
@@ -33,39 +33,46 @@ const submitFormHandler = async (event) => {
   const signupPw = document.getElementById("password-signup");
 
 
-  // add registration inputs to DB and send user to dashboard page
-  let registerBtn = document.getElementById("register-button")
 
-  registerBtn.addEventListener("click", async function(event) {
-  // check on this code:
-  event.preventDefault();
+    if (signupEmail.value && signupName.value && signupPw.value) {
+      // Send the e-mail and password to the server
+      const response = await fetch('/api/users/register', {
+        method: 'POST',
+        body: JSON.stringify({ name: signupName.value, email: signupEmail.value, password: signupPw.value }),
+        headers: { 'Content-Type': 'application/json' },
+      });
 
-if (signupEmail.value && signupName.value && signupPw.value) {
-  // Send the e-mail and password to the server
-  const response = await fetch('/api/users/register', {
-    method: 'POST',
-    body: JSON.stringify({ name: signupName.value, email: signupEmail.value, password: signupPw.value }),
-    headers: { 'Content-Type': 'application/json' },
-  });
+      if (response.ok) {
 
-  if (response.ok) {
-    
-    document.location.replace('/dashboard');
-  } else {
-    alert('Failed to log in');
-  }
-}
-})};
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to log in');
+      }
+    }
+  };
 
 // Verify user info and send to dashboard page
 let login = document.getElementById("submit-button")
 
-login.addEventListener("click", function() {
+login.addEventListener("click", function () {
   document.location.replace('/dashboard');
 
-})
+});
+
+  // add registration inputs to DB and send user to dashboard page
+  let registerBtn = document.getElementById("register-button")
+
+  registerBtn.addEventListener("click", async function (event) {
+    // check on this code:
+    document.location.replace('/dashboard');
+
+    event.preventDefault()});
+
 
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', submitFormHandler);
 
